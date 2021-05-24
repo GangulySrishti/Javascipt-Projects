3c3457fe610a264bd1737e3bd7f2237f7a25cf91
GangulySrishti <srishti.ganguly1999@gmail.com>
2021-05-25 00:59:34 +0530

    Added Delete and Toggle options to Shopping List

--- domevents/script.js
+++ domevents/script.js
@@ -1,14 +1,34 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var deleteButtons = document.getElementsByClassName("delete");
+
+
+for(var i=0; i<deleteButtons.length; i++){
+	deleteButtons[i].addEventListener("click", removeParent, false);
+}
+
+function removeParent(event){
+	event.target.removeEventListener("click", removeParent, false);
+	event.target.parentNode.remove();
+}
+
+ul.onclick=function(event){
+	event.target.classList.toggle("done");
+}
 
 function inputLength() {
 	return input.value.length;
 }
 
 function createListElement() {
+	var btn = document.createElement("button");
+	btn.innerHTML = "Delete";
+	btn.onclick = removeParent;
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
+	li.innerHTML = li.innerHTML + " ";
+	li.appendChild(btn);
 	ul.appendChild(li);
 	input.value = "";
 }
@@ -25,6 +45,6 @@ function addListAfterKeypress(event) {
 	}
 }
 
+ul.addEventListener("click", strikeThrough);
 button.addEventListener("click", addListAfterClick);
-
 input.addEventListener("keypress", addListAfterKeypress);
